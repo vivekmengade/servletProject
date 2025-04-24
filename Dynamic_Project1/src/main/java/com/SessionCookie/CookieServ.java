683181be824b9e0c95bf01d8dc95c00fd9c1904d
vivekmengade <vivekmengade@gmail.com>
2025-04-24 16:03:08 +0530

    using cookie learn

--- Dynamic_Project1/src/main/java/com/SessionCookie/CookieServ.java
+++ Dynamic_Project1/src/main/java/com/SessionCookie/CookieServ.java
@@ -7,6 +7,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CookieServ extends HttpServlet{
 	@Override
@@ -19,13 +20,33 @@ public class CookieServ extends HttpServlet{
 		String name= req.getParameter("uname");
 		writer.println("<h1>Nmae:- "+name+"</h1>");
 		
+//		Cookie Creation
+		
+//		Cookie c1=new Cookie("U_name", name);
+//		res.addCookie(c1);
+
+		
+		
+//		url  Rewriting
+		
+//		writer.println("<h3><a href='Cookie2?uname"+name+"'>GoToCookie2</a></h3>");
 		writer.println("<h3><a href='Cookie2'>GoToCookie2</a></h3>");
 		
 		
 		
-//		Cookie Creation
-		Cookie c1=new Cookie("U_name", name);
-		res.addCookie(c1);
+		
+//		Hidden form
+		
+//		writer.println("<form action='Cookie2'>"
+//				+ "<input type='hidden' name='u_name' value='"+name+"'>"
+//				+"<button type='submit'>Go To Servlet-2</button>"
+//				+"</form>");
+		
+		
+//		Http Session
+		HttpSession session= req.getSession();
+		session.setAttribute("u_name", name);
+		writer.println("<h1><a href='Cookie2'>Cookie</a></h1>");
 		
 		
 		
--- Dynamic_Project1/src/main/java/com/SessionCookie/CookieServ2.java
+++ Dynamic_Project1/src/main/java/com/SessionCookie/CookieServ2.java
@@ -3,9 +3,11 @@ package com.SessionCookie;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CookieServ2 extends HttpServlet{
 
@@ -14,6 +16,36 @@ public class CookieServ2 extends HttpServlet{
 		res.setContentType("text/html");
 		PrintWriter writer= res.getWriter();
 		writer.println("<h1>In Cookie Servelet 2</h1>");
+		
+		
+//		Cookie[] carry= req.getCookies();
+//		if(carry!=null) {
+//			writer.println("<h2>cookie found</h2>");
+//			
+//			
+//			for (Cookie c : carry) {
+//				String name=c.getName();
+//				writer.println("<h2>Cookie Name:- "+name+"</h2>");
+//				
+//				
+//				String value=c.getValue();
+//				writer.println("<h2>Value of Cookie: "+value+"</h2>");
+//				
+//			}
+//			
+//		}else {
+//			writer.println("<h1>You are a new user</h2>");
+//			
+//		}
+		
+//		String name= req.getParameter("u_name");
+//		writer.println("<h1>Name:- "+name+"</h1>");
+		
+		
+		
+		HttpSession session=req.getSession(true);
+		String n= (String) session.getAttribute("u_name");
+		writer.println("<h1>Name:- "+n+"</h1>");
 		
 	}
 	
